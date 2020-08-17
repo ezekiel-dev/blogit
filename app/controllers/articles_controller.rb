a0d1bfd40aeb52e2cb787e2b9cb2289a38c04844
vanheaven-ui <vanheaven6@gmail.com>
2020-08-17 08:18:13 +0300

    Add comments section

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def edit
@@ -20,6 +22,8 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article #{@article.title} created!"
+
     redirect_to article_path(@article)
   end
 
@@ -27,12 +31,16 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
+    flash.notice = "Article #{@article.title} deleted!"
+
     redirect_to articles_path
   end
 
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
+    
+    flash.notice = "Article #{@article.title} updated!"
 
     redirect_to article_path(@article)
   end
